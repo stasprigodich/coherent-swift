b650b215e87738dc582f0b799051a9afccc79614
Stan Prigodich <stan@backbase.com>
2023-11-07 15:40:07 +0100

    chore: update xcode and mac os versions and bump up to 0.5.12

--- Sources/CoherentSwift/main.swift
+++ Sources/CoherentSwift/main.swift
@@ -8,7 +8,7 @@ struct CoherentSwift: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "coherent-swift",
         abstract: "A command-line tool to analyze and report Swift code cohesion",
-        version: "0.5.11",
+        version: "0.5.12",
         subcommands: [
             Initializer.self,
             Report.self
